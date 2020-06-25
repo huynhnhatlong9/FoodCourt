083ae685b51d5290361c66382bbe9928aa359ae5
Long Huynh <huynhnhatlong9@gmail.com>
2020-06-25 21:27:59 +0700

    update

--- menu/views.py
+++ menu/views.py
@@ -19,17 +19,23 @@ class AddFoodView(CreateView):
 
 def menu_view(request):
     add = ''
-    food = Product.objects.all()
+    food = Product.objects.all().filter(quantity__gt=0)
     food_filter = FoodFilter(request.GET, queryset=food)
     food = food_filter.qs
     if request.GET:
         if 'add' in request.GET:
             add = request.GET['add']
-            if Cart.objects.filter(food_id=add, user_id=request.user.id):
-                Cart.objects.filter(food_id=add, user_id=request.user.id).update(
-                    quantity=Cart.objects.get(food_id=add, user_id=request.user.id).quantity + 1)
+            obj = Cart.objects.filter(food_id=add, user_id=request.user.id)
+            if obj:
+                obj.update(quantity=obj.first().quantity + 1)
+                obj = Cart.objects.all().get(food_id=add, user_id=request.user.id)
+                obj.food.quantity -= 1
+                obj.food.save()
             else:
                 Cart.objects.create(user=User.objects.get(id=request.user.id), food=Product.objects.get(id=add))
+                obj = Cart.objects.all().get(food_id=add, user_id=request.user.id)
+                obj.food.quantity -= 1
+                obj.food.save()
     context = {
         'foods': food,
         'filter': food_filter,
@@ -54,10 +60,15 @@ def cart_view(request):
             cl = request.POST.get('cl')
             obj = Cart.objects.all().get(user=request.user, id=cl)
             if obj.quantity == 1:
+                obj.food.quantity += 1
+                obj.food.save()
                 obj.delete()
+
             else:
                 obj.quantity -= 1
                 obj.save()
+                obj.food.quantity += 1
+                obj.food.save()
     food = Cart.objects.filter(user=request.user)
     context = {
         'foods': food,
