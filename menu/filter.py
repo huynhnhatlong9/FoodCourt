203aa0e4494349aa1aa1e37ac94cfc799d5c2a48
Long Huynh <huynhnhatlong9@gmail.com>
2020-06-27 13:25:33 +0700

    update pro

--- menu/filter.py
+++ menu/filter.py
@@ -1,13 +1,11 @@
-
+import django_filters
 from django_filters import FilterSet
 
 from home.models import Product
 
 
 class FoodFilter(FilterSet):
+    name=django_filters.CharFilter(label='Name: ',lookup_expr='icontains')
     class Meta:
         model = Product
-        fields = {
-            'category': ['exact'],
-            'name': ['exact']
-        }
+        fields = ['name','category']
